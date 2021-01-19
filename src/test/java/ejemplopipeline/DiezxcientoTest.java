45b7faf52c8fe97684c837f7d7f20461f4c03b04
Cristian <cristian.cubillos@editrade.cl>
2021-01-19 20:27:59 -0300

    Modificado

--- src/test/java/ejemplopipeline/DiezxcientoTest.java
+++ src/test/java/ejemplopipeline/DiezxcientoTest.java
@@ -1,6 +1,7 @@
 package ejemplopipeline;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
@@ -14,12 +15,15 @@ public class DiezxcientoTest {
 
 	@Test
 	public void testImpuesto() throws Exception {
-
+		Diezxciento diezxciento = new Diezxciento(23000000,900000);
+		assertEquals(0,diezxciento.impuesto());
 	}
 
 	@Test
 	public void testSaldoAhorro() throws Exception {
-
+		Diezxciento diezxciento = new Diezxciento(100000000,900000);
+		assertEquals(90000000,diezxciento.saldoAhorro());
+		assertNotEquals(1l,(long)diezxciento.saldoAhorro());
 	}
 
 }
